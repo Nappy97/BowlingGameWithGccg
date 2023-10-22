dda231fc4893d5a56b1f3ffaa2f1062492a9ccbb
정의형 <wjddmlgud829@gmail.com>
2023-10-22 20:35:23 +0900

    Gccg Template init

---
+++ BowlinGame.Gccg/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BowlingGame.Data.Data;
+
+Console.WriteLine("Hello, World!");
+Gccg.Core.Generator.Generate(new ChinookContext());
